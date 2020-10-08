7a5c21ccfd5f5747aacfca91cc138303f4e775dc
ybugge <yves.bugge@gmx.de>
2020-10-08 20:16:42 +0200

    test

--- Main.py
+++ Main.py
@@ -4,3 +4,12 @@ Created on 06.10.2020
 @author: ybugge
 '''
 print("Hallo")
+
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(10)
+camera.stop_preview()
