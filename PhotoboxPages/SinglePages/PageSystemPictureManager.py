67b3303cf1ebaa1ad81a6248182902dbd9de25bd
ybugge <yves.bugge@gmx.de>
2021-03-21 20:09:49 +0100

    test

--- PhotoboxPages/SinglePages/PageSystemPictureManager.py
+++ PhotoboxPages/SinglePages/PageSystemPictureManager.py
@@ -2,7 +2,7 @@ import os
 
 from PyQt5.QtCore import QTimer, QSize, Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QLabel, QProgressBar, QScrollArea
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QLabel, QProgressBar, QScrollArea, QWidget
 
 from PhotoboxPages.AllPages import AllPages
 from PhotoboxPages.Page import Page
@@ -15,39 +15,46 @@ from config.Config import TextKey, textValue, CfgKey
 class PageSystemPictureManager(Page):
     def __init__(self, pages : AllPages,windowSize:QSize):
         super().__init__(pages,windowSize)
+
+        mainLayout = QVBoxLayout()
+        self.setLayout(mainLayout)
+
+        #Titel
+        mainLayout.addWidget(self.getTitleAsQLabel(TextKey.PAGE_SYSTEMPICTUREMANAGER_TITLE))
+
+        scroll_area_content_widget = QWidget()
         self.scroll_area = QScrollArea(self)
         self.scroll_area.setGeometry(0, 0, windowSize.width(), windowSize.height())
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll_area.setWidget(scroll_area_content_widget)
 
-        mainLabel = QVBoxLayout()
-        self.scroll_area.setLayout(mainLabel)
-
-        #Titel
-        mainLabel.addWidget(self.getTitleAsQLabel(TextKey.PAGE_SYSTEMPICTUREMANAGER_TITLE))
+        mainContentLabel = QVBoxLayout()
+        scroll_area_content_widget.setLayout(mainContentLabel)
+        mainLayout.addWidget(self.scroll_area)
 
         #Funny pictures
         funnyTitle = QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_FUNNY_TITEL])
         funnyTitleFont = QFont()
         funnyTitleFont.setUnderline(True)
         funnyTitle.setFont(funnyTitleFont)
-        mainLabel.addWidget(funnyTitle)
+        mainContentLabel.addWidget(funnyTitle)
 
-        mainLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_SOURCELABEL]))
+        mainContentLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_SOURCELABEL]))
         funnySource = QLineEdit()
         funnySource.setEnabled(False)
         funnySource.setText(CfgService.get(CfgKey.PAGE_SYSTEMPICTUREMANAGER_FUNNY_PICTURE_SOURCE))
-        mainLabel.addWidget(funnySource)
+        mainContentLabel.addWidget(funnySource)
 
-        mainLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_TARGETLABEL]))
+        mainContentLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_TARGETLABEL]))
         funnyTarget = QLineEdit()
         funnyTarget.setEnabled(False)
         funnyTarget.setText(CfgService.get(CfgKey.PAGE_CAPTUREPHOTO_LAST_IMAGE_FOLDER))
-        mainLabel.addWidget(funnyTarget)
+        mainContentLabel.addWidget(funnyTarget)
 
         funnyPictureNavigation = QHBoxLayout()
-        mainLabel.addLayout(funnyPictureNavigation)
+        mainContentLabel.addLayout(funnyPictureNavigation)
 
         self.funnyDeleteButton = QPushButton(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_DELETEBUTTON])
         self.funnyDeleteButton.clicked.connect(self.deleteFunnyPictureFolder)
@@ -62,25 +69,25 @@ class PageSystemPictureManager(Page):
         loadingGifFont = QFont()
         loadingGifFont.setUnderline(True)
         loadingGifTitle.setFont(funnyTitleFont)
-        mainLabel.addWidget(loadingGifTitle)
+        mainContentLabel.addWidget(loadingGifTitle)
 
-        mainLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_SOURCELABEL]))
+        mainContentLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_SOURCELABEL]))
         loadingGifSource = QLineEdit()
         loadingGifSource.setEnabled(False)
         loadingGifSource.setText(CfgService.get(CfgKey.PAGE_SYSTEMPICTUREMANAGER_LOADINGGIFS_PICTURE_SOURCE))
-        mainLabel.addWidget(loadingGifSource)
+        mainContentLabel.addWidget(loadingGifSource)
 
-        mainLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_TARGETLABEL]))
+        mainContentLabel.addWidget(QLabel(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_TARGETLABEL]))
         loadingGifTarget = QLineEdit()
         loadingGifTarget.setEnabled(False)
         loadingGifTarget.setText(CfgService.get(CfgKey.PAGE_CAPTUREPHOTO_LOADING_GIF_FOLDER))
-        mainLabel.addWidget(loadingGifTarget)
+        mainContentLabel.addWidget(loadingGifTarget)
 
         loadingGifNavigation = QHBoxLayout()
-        mainLabel.addLayout(loadingGifNavigation)
+        mainContentLabel.addLayout(loadingGifNavigation)
 
         #Test############################################################################################################
-        mainLabel.addWidget(self.getTitleAsQLabel(TextKey.PAGE_SYSTEMPICTUREMANAGER_TITLE))
+        mainContentLabel.addWidget(self.getTitleAsQLabel(TextKey.PAGE_SYSTEMPICTUREMANAGER_TITLE))
 
         self.loadingGifDeleteButton = QPushButton(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_DELETEBUTTON])
         self.loadingGifDeleteButton.clicked.connect(self.deleteLoadingGifFolder)
@@ -91,14 +98,14 @@ class PageSystemPictureManager(Page):
         loadingGifNavigation.addWidget(self.loadingGifUpdateButton)
 
 
-        mainLabel.addStretch()
+        mainContentLabel.addStretch()
         #progressbar
         self.progressbar = QProgressBar()
         self.progressbar.setValue(0)
-        mainLabel.addWidget(self.progressbar)
+        mainContentLabel.addWidget(self.progressbar)
         #Navigationbuttons
         navigationBox = QHBoxLayout()
-        mainLabel.addLayout(navigationBox)
+        mainLayout.addLayout(navigationBox)
 
         self.pictureManagerButton = QPushButton(textValue[TextKey.PAGE_SYSTEMPICTUREMANAGER_NEXTBUTTON])
         self.pictureManagerButton.clicked.connect(self.nextPageEvent)
