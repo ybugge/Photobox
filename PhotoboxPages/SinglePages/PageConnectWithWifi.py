425bc44a6cfa60a5e8e7c821043a6e93c65fe0e7
ybugge <yves.bugge@gmx.de>
2021-08-28 15:01:57 +0200

    devide downlaodpage: Qr-Code for Wifi in extra page

--- PhotoboxPages/SinglePages/PageConnectWithWifi.py
+++ PhotoboxPages/SinglePages/PageConnectWithWifi.py
@@ -12,18 +12,19 @@ from Services.CfgService import CfgService
 from Services.GlobalPagesVariableService import GlobalPagesVariableService
 from config.Config import TextKey, textValue, CfgKey
 
-class PageDownloadPicture(Page):
+class PageConnectWithWifi(Page):
     def __init__(self, pages : AllPages, windowsize:QSize, globalVariable:GlobalPagesVariableService):
         super().__init__(pages,windowsize)
         self.windowsize = windowsize
         self.globalVariable = globalVariable
+        self.switch = False
 
         mainLayout = QVBoxLayout()
         mainLayout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(mainLayout)
 
         #Titel
-        self.title = self.getTitleAsQLabel(TextKey.PAGE_DOWNLOADPICTURE_TITLE)
+        self.title = self.getTitleAsQLabel(TextKey.PAGE_DOWNLOADPICTURE_WIFI_TITLE)
         mainLayout.addWidget(self.title)
         mainLayout.addStretch()
 
@@ -43,24 +44,32 @@ class PageDownloadPicture(Page):
         self.setNavigationbuttonStyle(backButton)
         navigationLayout.addWidget(backButton)
 
-        nextButton = QPushButton(textValue[TextKey.PAGE_DOWNLOADPICTURE_WIFI_TITLE])
-        nextButton.clicked.connect(self.nextPageEvent)
-        self.setNavigationbuttonStyle(nextButton)
-        navigationLayout.addWidget(nextButton)
-
     def executeBefore(self):
         self.updateQrCodePicture()
         self.globalVariable.setPictureUsed(True)
 
     def updateQrCodePicture(self):
         #QRCode
-        data = "http://"+CfgService.get(CfgKey.SERVER_IP)+":"+CfgService.get(CfgKey.SERVER_PORT)+CfgService.get(CfgKey.SERVER_DOWNLOAD_PICTURE_PAGE)+"/"+self.globalVariable.getPictureSubName()
+
+        data = 'WIFI:S:{};T:{};P:{};;'.format(CfgService.get(CfgKey.WIFI_SSID),CfgService.get(CfgKey.WIFI_PROTOCOL),CfgService.get(CfgKey.WIFI_PASSWORD))
 
         buf = io.BytesIO()
         qr_code = qrcode.make(data)
         img = Image.new("RGB", (qr_code.pixel_size,qr_code.pixel_size), "white")
         img.paste(qr_code,(0,0))
         img = img.resize((qr_code.pixel_size*2,qr_code.pixel_size*2))
+
+
+        fontSize = 40
+        myFont = ImageFont.truetype(CfgService.get(CfgKey.WIFI_QR_CODE_FONT), fontSize)
+        title = ImageDraw.Draw(img)
+        title.text((20, 20), textValue[TextKey.QR_CODE_WIFI_NAME]+CfgService.get(CfgKey.WIFI_SSID),font=myFont, fill=(0, 0, 0))
+
+        password = ImageDraw.Draw(img)
+        witheSpaceNumber = 4
+        unformatedPasswordAsString = CfgService.get(CfgKey.WIFI_PASSWORD)
+        formatedPasswordAsString = " ".join(unformatedPasswordAsString[i:i+witheSpaceNumber] for i,c in enumerate(unformatedPasswordAsString) if i % witheSpaceNumber == 0)
+        password.text((20, qr_code.pixel_size*2-20-fontSize), textValue[TextKey.QR_CODE_WIFI_PASSWORD]+formatedPasswordAsString,font=myFont, fill=(0, 0, 0))
         img.save(buf, "PNG")
 
         qt_pixmap = QPixmap()
